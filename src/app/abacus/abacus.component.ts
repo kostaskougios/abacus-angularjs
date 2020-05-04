c8d90a701f8a7240acbf529ba475fa2334adff51
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2020-05-04 11:35:40 +0100

    -

--- src/app/abacus/abacus.component.ts
+++ src/app/abacus/abacus.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbacusComponent implements OnInit {
 
-  rows = [3, 5, 7, 2, 3, 4, 5, 6, 7, 5];
+  rows = [3, 5, 7, 2, 3, 4, 5, 6, 7, 10];
 
   constructor() { }
 
@@ -18,6 +18,13 @@ export class AbacusComponent implements OnInit {
     let x = [];
     var i = 1;
     while (x.push(i++) < row) { };
+    return x;
+  }
+
+  rightOfRow(row: number): Array<number> {
+    let x = [];
+    var i = row + 1;
+    if (i < 11) while (x.push(i++) < 10 - row) { };
     return x;
   }
 
