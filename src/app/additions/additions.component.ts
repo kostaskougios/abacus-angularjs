8041e320fd464398a309e34d3aab3b172c330365
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2020-05-11 12:18:01 +0100

    -

--- src/app/additions/additions.component.ts
+++ src/app/additions/additions.component.ts
@@ -23,4 +23,19 @@ export class AdditionsComponent implements OnInit {
   isCorrect(row): boolean {
     return row.a + row.b == row.r;
   }
+
+  newRow(max: number): void {
+    var a = max, b = max;
+    while (a + b > max) {
+      a = Math.floor(Math.random() * max) + 1;
+      b = Math.floor(Math.random() * max) + 1;
+    }
+    this.rows.unshift(
+      {
+        a: a,
+        b: b,
+        r: 0
+      }
+    );
+  }
 }
