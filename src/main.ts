b6fb517725cd07c704c9e10638726151214d1f0d
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2020-05-12 14:21:04 +0100

    -

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,23 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
 if (environment.production) {
+  let ga_id = "UA-25501899-9"
+
+  document.write(`<script async src="https://www.googletagmanager.com/gtag/js?id=${ga_id}"></script>`);
+
+  const script1 = document.createElement('script');
+  script1.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${ga_id}', {
+            'linker': {
+            'domains': ['abacus-64f90.firebaseapp.com']
+            }
+        });
+    `;
+  document.head.appendChild(script1);
+
   enableProdMode();
 }
 
