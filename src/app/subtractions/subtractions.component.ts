241c3451b5d76f0238ec64b0c6bdd4c525f75eef
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2020-05-12 10:48:24 +0100

    -

--- src/app/subtractions/subtractions.component.ts
+++ src/app/subtractions/subtractions.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubtractionsComponent implements OnInit {
 
+  rows = [
+  ];
+
+  isAbacus = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isCorrect(row): boolean {
+    return row.r != "" && (row.a - row.b == row.r);
+  }
+
+  isIncorrect(row): boolean {
+    return row.r != "" && !this.isCorrect(row)
+  }
+
+  newRow(max: number): void {
+    var a = 1, b = 2;
+    while (a - b < 0) {
+      a = Math.floor(Math.random() * max) + 1;
+      b = Math.floor(Math.random() * max) + 1;
+    }
+    this.rows.unshift(
+      {
+        a: a,
+        b: b,
+        r: ""
+      }
+    );
+  }
+
+  abacus(): void {
+    this.isAbacus = !this.isAbacus;
   }
 
 }
