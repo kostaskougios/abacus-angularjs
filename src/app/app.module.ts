2912aeae7a564a3ac38dc2cb3660e8ac8e7b7155
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2020-05-12 12:11:25 +0100

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -10,18 +13,23 @@ import { AbacusComponent } from './abacus/abacus.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AdditionsComponent } from './additions/additions.component';
 import { SubtractionsComponent } from './subtractions/subtractions.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatButtonModule,
     RouterModule.forRoot([
       { path: '', component: HomePageComponent },
       { path: 'abacus', component: AbacusComponent },
       { path: 'additions', component: AdditionsComponent },
       { path: 'subtractions', component: SubtractionsComponent },
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
